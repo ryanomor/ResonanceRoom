3ddf3646698f0a227c711f1528e998d9a47d139c
ryanomor <32519935+ryanomor@users.noreply.github.com>
2026-03-01 21:33:37 -0500

    Updated .gitignore

---
+++ .expo/types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string = string> extends Record<string, unknown> {
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/home` | `/(tabs)/matches` | `/(tabs)/notifications` | `/(tabs)/profile` | `/_sitemap` | `/auth` | `/auth/login` | `/auth/signup` | `/home` | `/matches` | `/notifications` | `/profile` | `/room/create`;
+      DynamicRoutes: `/chat/${Router.SingleRoutePart<T>}` | `/game/${Router.SingleRoutePart<T>}` | `/room/${Router.SingleRoutePart<T>}`;
+      DynamicRouteTemplate: `/chat/[id]` | `/game/[id]` | `/room/[id]`;
+    }
+  }
+}
