53d066733fc18aa49a152f44f7f2b81ac4d2d978
ryanomor <32519935+ryanomor@users.noreply.github.com>
2026-03-22 20:30:44 -0400

    Updated router.d.ts

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -7,8 +7,8 @@ declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
       StaticRoutes: `/` | `/(tabs)` | `/(tabs)/home` | `/(tabs)/matches` | `/(tabs)/notifications` | `/(tabs)/profile` | `/_sitemap` | `/auth` | `/auth/login` | `/auth/signup` | `/auth/social-profile` | `/home` | `/matches` | `/notifications` | `/profile` | `/room/create`;
-      DynamicRoutes: `/chat/${Router.SingleRoutePart<T>}` | `/game/${Router.SingleRoutePart<T>}` | `/room/${Router.SingleRoutePart<T>}`;
-      DynamicRouteTemplate: `/chat/[id]` | `/game/[id]` | `/room/[id]`;
+      DynamicRoutes: `/chat/${Router.SingleRoutePart<T>}` | `/game/${Router.SingleRoutePart<T>}` | `/room/${Router.SingleRoutePart<T>}` | `/user/${Router.SingleRoutePart<T>}`;
+      DynamicRouteTemplate: `/chat/[id]` | `/game/[id]` | `/room/[id]` | `/user/[id]`;
     }
   }
 }
